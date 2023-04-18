dca3318e3481b763f22205d3e8bd31ac478eb143
pranavkonde <pranavkonde2020@gmail.com>
2023-04-18 16:58:42 +0530

    Initialize React

---
+++ client1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+    <Route exact path="/" element={<Home />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
